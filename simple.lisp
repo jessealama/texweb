e409db0f65b5d8a37e3f3ebace3e92cf7f329d84
Jesse Alama <alama@pequeno.(none)>
2010-04-06 21:37:45 +0100

    Set to prologue to XHTML 1.1.

--- simple.lisp
+++ simple.lisp
@@ -13,6 +13,9 @@
       `(,(create-prefix-dispatcher "/test" 'test-page)
 	,(create-prefix-dispatcher "/about" 'about-page)))
 
+(setq *prologue*
+      "<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.1//EN\" \"http://www.w3.org/TR/xhtml1/DTD/xhtml11.dtd\">")
+
 (defmacro with-xml-declaration (&body body)
   `(concatenate 'string
      "<?xml version='1.0' encoding='UTF-8'?>"
